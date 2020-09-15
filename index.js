44aa701f4509a18e5b80cd89d3d410a465981f04
calcala <c.alcala@uniandes.edu.co>
2020-09-15 18:04:52 -0500

    proveedores

--- index.js
+++ index.js
@@ -2,37 +2,47 @@ const axios = require('axios');
 var http = require('http');
 const fs = require('fs');
 const url = require('url'); 
+const { info } = require('console');
+
+let htmlInicio = '<!DOCTYPE html> <html lang="en"> <head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><title>NodeJS</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"></head><body><table class="table table-striped"><h1 class="text-center">Listado de ';
+let htmlMedio = '</h1><thead><tr><th scope="col">ID</th><th scope="col">Nombre</th><th scope="col">Contacto</th></tr></thead><tbody id="table">';
+let htmlFin = '</tbody></table></body></html>';
 
 let urlProveedores = "https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json";
 let urlClientes = "https://gist.githubusercontent.com/josejbocanegra/986182ce2dd3e6246adcf960f9cda061/raw/f013c156f37c34117c0d4ba9779b15d427fb8dcd/clientes.json";
 
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    let filename ="";
-    if (q.pathname.search('proveedores')==1) filename = 'proveedores';
-    else if (q.pathname.search('clientes')==1) filename = 'clientes';
+let promiseProveedores = axios.get(urlProveedores);
+let promiseClientes = axios.get(urlClientes);
 
-    fs.readFile("index.html", function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }
+Promise.all([promiseClientes, promiseProveedores]).then(resp=> {
 
-        adapt(filename);
+    http.createServer(function (req, res) {
+        var q = url.parse(req.url, true);
+        let filename ="";
+        if (q.pathname.search('proveedores')==1) filename = 'proveedores';
+        else if (q.pathname.search('clientes')==1) filename = 'clientes';
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    })
+        fs.readFile(filename + ".html", function(err, data) {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end("404 Not Found");
+            }
 
-}).listen(8081); 
+            let promise = filename==="proveedores"? resp[1] : resp[0];
+            let info = promise.data;
+            console.log(info);
 
-function adapt(filename) {
-    let url = filename==="proveedores"? urlProveedores : urlClientes;
-    axios.get(url);
-    let document = "/index.html";
-    document.getElementById("tipo").innerHTML("Listado de " + filename);
-    Promise.then(res => res.json()).then(info => {
+            let html = "";
+            for (let i=0; i < info.length; i++) {
+                let id = filename==="proveedores"? info[i].idproveedor : info[i].idcliente;
+                html += "<tr><td>" + id + "</td><td>"+ info[i].nombrecompania +"</td><td>"+ info[i].nombrecontacto +"</td></tr>";
+            }
 
-    });
-}
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(htmlInicio + filename + htmlMedio + html + htmlFin);
+            return res.end();
+        })
+
+    }).listen(8081); 
+    
+});
